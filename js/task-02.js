c413f03275b3f8a2ce2890c23d026efa1f37ad3a
Dmitry-Shestakov <goldgarrincha@gmail.com>
2022-11-05 13:02:53 +0300

    done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
+// const list = document.querySelector("#ingredients");
+
+// let newList = [];
+
+// ingredients.forEach((element) => {
+//   const listElement = document.createElement("li");
+//   listElement.classList.add("item");
+//   listElement.textContent = element;
+//   newList.push(listElement);
+// });
+
+// list.append(...newList);
